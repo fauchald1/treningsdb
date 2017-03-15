db86e1c854691cfb8976d25564b00968a4d3cd89
Oystein Knutsen <oystein@knutsen.io>
2017-03-15 18:13:25 +0100

    Basic reading of DB added

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,151 +1,45 @@
-package application;
+package application;   
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ObjectPropertyBase;
-import javafx.beans.property.Property;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+public class Main
+{
+  public static void main(String[] args) throws ClassNotFoundException
+  {
+    // Load sqlite-JDBC driver
+    Class.forName("org.sqlite.JDBC");
 
-public class Appointment {
+    Connection connection = null;
+    try
+    {
+     // create a DB connection
+     connection = DriverManager.getConnection("jdbc:sqlite:trening.db");
 
-	private StringProperty formalProperty = new SimpleStringProperty();
-	private StringProperty romProperty = new SimpleStringProperty();
-	private IntegerProperty repetisjonProperty = new SimpleIntegerProperty();
-	private Property<LocalDate> datoProperty = new ObjectPropertyBase<LocalDate>(null) {
+     Statement statement = connection.createStatement();
+         statement.setQueryTimeout(30);  // Timeout is 30s
 
-		@Override
-		public Object getBean() {
-			return this;
-		}
+         ResultSet resultSet = statement.executeQuery("SELECT * from Kategori");
+         while(resultSet.next())
+         {
+          // iterate results
+          System.out.println("name = " + resultSet.getString("Navn"));
+          System.out.println("id = " + resultSet.getInt("KategoriID"));
+        }
+      }
 
-		@Override
-		public String getName() {
-			return "dato";
-		}
-	};
-	private Property<LocalTime> fraProperty = new ObjectPropertyBase<LocalTime>(null) {
-
-		@Override
-		public Object getBean() {
-			return this;
-		}
-
-		@Override
-		public String getName() {
-			return "fra";
-		}
-	};
-	private Property<LocalTime> tilProperty = new ObjectPropertyBase<LocalTime>(null) {
-
-		@Override
-		public Object getBean() {
-			return this;
-		}
-
-		@Override
-		public String getName() {
-			return "til";
-		}
-	};
-	private Property<LocalDate> sluttProperty = new ObjectPropertyBase<LocalDate>(null) {
-
-		@Override
-		public Object getBean() {
-			return this;
-		}
-
-		@Override
-		public String getName() {
-			return "slutt";
-		}
-	};
-
-	public String getFormal() {
-		return formalProperty.getValue();
-	}
-
-	public void setFormal(String formal) {
-		formalProperty.setValue(formal);
-	}
-
-	public StringProperty formalProperty() {
-		return formalProperty;
-	}
-
-	public String getRom() {
-		return romProperty.getValue();
-	}
-
-	public void setRom(String rom) {
-		romProperty.setValue(rom);
-	}
-
-	public StringProperty romProperty() {
-		return romProperty;
-	}
-
-	public LocalDate getDato() {
-		return datoProperty.getValue();
-	}
-
-	public void setDato(LocalDate dato) {
-		datoProperty.setValue(dato);
-	}
-
-	public Property<LocalDate> DatoProperty() {
-		return datoProperty;
-	}
-
-	public LocalTime getFra() {
-		return fraProperty.getValue();
-	}
-
-	public void setFra(LocalTime fra) {
-		fraProperty.setValue(fra);
-	}
-
-	public Property<LocalTime> fraProperty() {
-		return fraProperty;
-	}
-
-	public LocalTime getTil() {
-		return tilProperty.getValue();
-	}
-
-	public void setTil(LocalTime til) {
-		tilProperty.setValue(til);
-	}
-
-	public Property<LocalTime> tilProperty() {
-		return tilProperty;
-	}
-
-	public Integer getRepetisjon() {
-		return repetisjonProperty.getValue();
-	}
-
-	public void setRepetisjon(Integer repetisjon) {
-		repetisjonProperty.setValue(repetisjon);
-	}
-
-	public IntegerProperty repetisjonProperty() {
-		return repetisjonProperty;
-	}
-
-	public LocalDate getSlutt() {
-		return sluttProperty.getValue();
-	}
-
-	public void setSlutt(LocalDate slutt) {
-		sluttProperty.setValue(slutt);
-	}
-
-	public Property<LocalDate> sluttProperty() {
-		return sluttProperty;
-	}
-
-}
+      catch(SQLException e){  System.err.println(e.getMessage()); }       
+      finally {         
+        try {
+          if(connection != null)
+           connection.close();
+       }
+            catch(SQLException e) {         
+             System.err.println(e); 
+           }
+         }
+       }
+     }
