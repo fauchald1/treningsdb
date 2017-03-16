74cb5871bcdc0c65252bc2c167c038b18a3e3008
Oystein Knutsen <oystein@knutsen.io>
2017-03-16 16:42:07 +0100

    Fixed indents

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,49 +3,48 @@ package application;
 import java.util.Scanner;
 
 class Session {
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 		
-		DB db = new DB();
-        db.getKategori();
+	  DB db = new DB();
+	  db.getKategori();
 		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Velokmmen til din dagbok! \nKommandoer:");
+	  Scanner scanner = new Scanner(System.in);
+	  System.out.println("Velokmmen til din dagbok! \nKommandoer:");
 		
-		while (scanner.hasNextLine()) {
-		    String line = scanner.nextLine();
+	  while (scanner.hasNextLine()) {
+	    String line = scanner.nextLine();
 		    
-		    if(line.equals("ny")) {
-				System.out.println("\nDato:\n");
-				String dato = scanner.nextLine();
-				System.out.println("\nTidspunkt:\n");
-				String tid = scanner.nextLine();
+	    if(line.equals("ny")) {
+	      System.out.println("\nDato:\n");
+	      String dato = scanner.nextLine();
+	      System.out.println("\nTidspunkt:\n");
+	      String tid = scanner.nextLine();
 				System.out.println("\nVarighet:\n");
 				String varighet = scanner.nextLine();
 				System.out.println("\nNotat:\n");
 				String notat = scanner.nextLine();
 				System.out.println("\nAntall øvelser:\n");
-				String antall = scanner.nextLine();
+			  String antall = scanner.nextLine();
 				
-				for (int i = 0; i < Integer.parseInt(antall); i++) {
-					System.out.println("\nØvelse:\n");
-					String ovelse = scanner.nextLine();
-					System.out.println(ovelse + "\n");
-				}
-				
-		    }
-		    else if(line.equals("best")) {
-				System.out.println("Beste trening:\n");
-		    }
-		    else if(line.equals("stats")) {
-		    	System.out.println("Beste trening:\n");
-		    }
-		    else if(line.equals("slutt")) {
-		    	String file = scanner.nextLine();
-		    	System.out.println(file);
-		    }
-		    else {
-		    	System.out.println("Ugyldig kommando!\n");
-		    }
-		}
+			  for (int i = 0; i < Integer.parseInt(antall); i++) {
+			    System.out.println("\nØvelse:\n");
+			    String ovelse = scanner.nextLine();
+			    System.out.println(ovelse + "\n");
+			  }
+		  }
+	    else if(line.equals("best")) {
+	      System.out.println("Beste trening:\n");
+	      }
+	    else if(line.equals("stats")) {
+	      System.out.println("Beste trening:\n");
+	    }
+	    else if(line.equals("slutt")) {
+	      String file = scanner.nextLine();
+	      System.out.println(file);
+	    }
+	    else {
+	      System.out.println("Ugyldig kommando!\n");
+	    }
+	  }
 	}
 }
